8ddab3936e5b32556b6df208a0239273f598b033
vishwamtiwari <tiwarivishwam82@gmail.com>
2026-08-05 11:07:04 +0530

    Rotate Array

---
+++ array-string/189. Rotate Array/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int len = nums.length;
+        k = k % len;
+        if (k == 0) return;
+
+        reverse(nums, 0, len - 1);
+
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, len - 1);
+    }
+
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[end];
+            nums[end] = nums[start];
+            nums[start] = temp;
+            start++;
+            end--;
+        }
+    }
+}
