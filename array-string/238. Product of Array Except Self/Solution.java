4a89ef7275b3056b0ab1d9d52def74c4691c5360
vishwamtiwari <tiwarivishwam82@gmail.com>
2026-08-10 21:38:08 +0530

    Product of Array Except Self

---
+++ array-string/238. Product of Array Except Self/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] result = new int[nums.length];
+
+        int leftProduct = 1, rightProduct = 1;
+        for (int i = 0; i < nums.length; i++) {
+            result[i] = leftProduct;
+            leftProduct *= nums[i];
+        }
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+            result[i] = result[i] * rightProduct;
+            rightProduct *= nums[i];
+        }
+
+        return result;
+    }
+}
