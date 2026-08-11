b9fcdf4539383e669eefc24835c53d113451e4b6
vishwamtiwari <tiwarivishwam82@gmail.com>
2026-08-11 20:22:08 +0530

    Integer to Roman

---
+++ array-string/12. Integer to Roman/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String intToRoman(int num) {
+        int[] values = new int[] {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] symbols = new String[] {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        StringBuilder res = new StringBuilder();
+
+        for (int i = 0; i < values.length; i++) {
+            while (num >= values[i]) {
+                res.append(symbols[i]);
+                num -= values[i];
+            }
+        }
+        return res.toString();
+    }
+}
