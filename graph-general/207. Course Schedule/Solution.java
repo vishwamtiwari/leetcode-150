bbf04667b2d1b91eb0e7deb881ddd748dd6b66c2
vishwamtiwari <tiwarivishwam82@gmail.com>
2026-09-11 16:50:28 +0530

    Course Schedule

--- graph-general/207. Course Schedule/Solution.java
+++ graph-general/207. Course Schedule/Solution.java
@@ -1,34 +1,81 @@
 class Solution {
+//    public boolean canFinish(int numCourses, int[][] prerequisites) {
+//
+//        HashMap<Integer, List<Integer>> adjList = new HashMap<>();
+//
+//        for (int[] prerequisite: prerequisites) {
+//            int dependsOn = prerequisite[1];
+//            int dependent = prerequisite[0];
+//
+//            adjList.computeIfAbsent(dependsOn, k -> new ArrayList<>()).add(dependent);
+//        }
+//
+//        int[] state = new int[numCourses];
+//        for (int course = 0; course < numCourses; course++) {
+//            if (!dfs(course, adjList, state)) {
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
+//
+//    private boolean dfs(int course, Map<Integer, List<Integer>> adjList, int[] state) {
+//        if (state[course] == 1) return false;
+//        if (state[course] == 2) return true;
+//
+//        state[course] = 1;
+//        for (int i: adjList.getOrDefault(course, Collections.emptyList())) {
+//            if (!dfs(i, adjList, state))
+//                return false;
+//        }
+//        state[course] = 2;
+//        return true;
+//    }
+
+    /*
+        Kahn's algorithm
+        topological sort through BFS - cycle detection
+     */
     public boolean canFinish(int numCourses, int[][] prerequisites) {
+        HashMap<Integer, List<Integer>> adj = new HashMap<>();
+        int[] indegrees = new int[numCourses];
 
-        HashMap<Integer, List<Integer>> adjList = new HashMap<>();
-
+        // creating adj list and indegress
         for (int[] prerequisite: prerequisites) {
-            int dependsOn = prerequisite[1];
-            int dependent = prerequisite[0];
+            int u = prerequisite[1];
+            int v = prerequisite[0];
 
-            adjList.computeIfAbsent(dependsOn, k -> new ArrayList<>()).add(dependent);
+            if (!adj.containsKey(u))
+                adj.put(u, new ArrayList<>());
+
+            adj.get(u).add(v);
+            indegrees[v]++;
         }
 
-        int[] state = new int[numCourses];
-        for (int course = 0; course < numCourses; course++) {
-            if (!dfs(course, adjList, state)) {
-                return false;
+        int count = 0;
+        Queue<Integer> queue = new ArrayDeque<>();
+
+        // adding elements with no prior dependencies to BFS queue
+        for (int u = 0; u < numCourses; u++) {
+            if (indegrees[u] == 0) {
+                queue.offer(u);
+                count++;
             }
         }
-        return true;
-    }
 
-    private boolean dfs(int course, Map<Integer, List<Integer>> adjList, int[] state) {
-        if (state[course] == 1) return false;
-        if (state[course] == 2) return true;
+        while (!queue.isEmpty()) {
+            int u = queue.poll();
 
-        state[course] = 1;
-        for (int i: adjList.getOrDefault(course, Collections.emptyList())) {
-            if (!dfs(i, adjList, state))
-                return false;
+            for (int v: adj.getOrDefault(u, Collections.emptyList())) {
+                indegrees[v]--;
+
+                if (indegrees[v] == 0) {
+                    queue.offer(v);
+                    count++;
+                }
+            }
         }
-        state[course] = 2;
-        return true;
+
+        return count == numCourses;
     }
 }
