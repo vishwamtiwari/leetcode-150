a09c503c279c750aba60a6a386648ea3de5cb702
vishwamtiwari <tiwarivishwam82@gmail.com>
2026-09-26 11:54:06 +0530

    Find Peak Element

---
+++ binary-search/162. Find Peak Element/Solution.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int len = nums.length;
+
+        if (len == 1)
+            return 0;
+
+        if (len == 2) {
+            if (nums[0] > nums[1])
+                return 0;
+            if (nums[0] < nums[1])
+                return 1;
+        }
+
+        if (nums[0] > nums[1])
+            return 0;
+        if (nums[len - 1] > nums[len - 2])
+            return len - 1;
+
+        int left = 0;
+        int right = len - 1;
+
+        while (left < right) {
+            int mid = left + (right - left)/2;
+
+            if (nums[mid] < nums[mid+1])
+                left = mid + 1;
+            else if (nums[mid] > nums[mid+1])
+                right = mid;
+        }
+
+        return left;
+    }
+}
